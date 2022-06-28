fb3a740fa7741ccb163d92f6e501f455209b09b9
Su Zhang <suzhang79@gmail.com>
2022-06-28 16:51:19 +1000

    add categories seed file

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+categories = ["coat", "jacket", "sweater", "top/shirt", "T-shirt", "polo", "hoody", "dress", "skirt", "shorts", "pants", "jeans", "shoes", "hat", "bag", "accessory"]
+
+if Category.count == 0
+    categories.each do |category|
+        Category.create(name: category)
+        puts "created #{category} category"
+    end
+end
