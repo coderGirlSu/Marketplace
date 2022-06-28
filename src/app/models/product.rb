a0e1b0b6301b4b3eab83e00d8f3cefa942a90ceb
Su Zhang <suzhang79@gmail.com>
2022-06-28 16:36:27 +1000

    add condition enum

--- src/app/models/product.rb
+++ src/app/models/product.rb
@@ -2,4 +2,14 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_rich_text :description
+
+  enum condition: {
+    as_new: 1,
+    near_perfect_mint: 2,
+    excellent: 3,
+    very_good: 4,
+    poor: 5
+  }
+
+  
 end
