0934204c17a36108016eca63778d97cba894092c
Su Zhang <suzhang79@gmail.com>
2022-06-29 16:15:18 +1000

    added helper to format conditions

--- src/app/helpers/products_helper.rb
+++ src/app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+    def format_condtion(condition)
+        words = condition.split("_").map do |word| 
+            word.capitalize
+        end
+        words.join(" ")
+    end
 end
