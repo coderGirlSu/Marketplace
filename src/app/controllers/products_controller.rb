6f2cf7714fe6d8a5a044c31e66bb087d9e4f35c8
Su Zhang <suzhang79@gmail.com>
2022-06-29 17:00:16 +1000

    implement authentication

--- src/app/controllers/products_controller.rb
+++ src/app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
   before_action :set_form_vars, only: %i[ new edit ]
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /products or /products.json
   def index
     @products = Product.all
