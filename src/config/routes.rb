2c04e1ace81ed58d0e3c53ffc372cebcbb966b29
Su Zhang <suzhang79@gmail.com>
2022-06-29 14:16:17 +1000

    added guide page

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   root 'pages#home'
+  get 'pages/guide'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
